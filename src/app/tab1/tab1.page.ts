59a0d1beeb22414f33753523e860e9b8d1f8754d
Carlos Ramart <carlos.ramart@gmail.com>
2019-01-01 18:13:01 -0300

    First commit with some styling and layout of the project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
-export class Tab1Page {}
+export class Tab1Page {
+  todos: any[] = [
+    {
+      title: "Douglas  Pace",
+      subtitle: "Douglas  Pace",
+      task: "Douglas  Pace",
+      done: false
+    },
+    {
+      title: "Douglas  Pace",
+      subtitle: "Douglas  Pace",
+      task: "Douglas  Pace",
+      done: true
+    }
+  ];
+}
